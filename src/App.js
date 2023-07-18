2939891ac4f4f3ddbaa2774f05e8d5ac892970c9
Tyler <tyler.d.lampel@gmail.com>
2023-07-18 17:38:45 -0400

    Toggle showForm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +26,13 @@ function App() {
     },
   ]);
 
+  // Add Task
+  function addTask(task) {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete Task
   function deleteTask(id) {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -41,8 +49,8 @@ function App() {
 
   return (
     <div className="Container">
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
